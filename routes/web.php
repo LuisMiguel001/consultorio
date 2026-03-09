fcbb0710cc080957039f27a4086bd6866e392e27
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2026-03-09 03:51:37 -0400

    Agregando metodo get para el login

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
